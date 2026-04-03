1f1ea6f6811f514c1c8759aa161c77207aae68e1
Guru07K <gnanagurubb55@email.com>
2026-04-03 13:07:52 +0530

    cors updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 7070;
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }))
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
