0fa549734c304b5441de0266f5967a359fdd440d
Guru07K <gnanagurubb33@gmail.com>
2026-04-12 20:00:24 +0530

    fix

--- backend/src/service/Firebase/firebase_config.ts
+++ backend/src/service/Firebase/firebase_config.ts
@@ -2,7 +2,7 @@ export const firbaseConfig = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY as string,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
