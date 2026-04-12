573469979cec02e3dd8c9bc8d58bde692076321f
Guru07K <gnanagurubb33@gmail.com>
2026-04-12 20:20:37 +0530

    FIX

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -36,7 +36,9 @@ self.addEventListener("notificationclick", function (event) {
         for (const tab of tabList) {
           if (tab.url.includes(self.location.origin) && "focus" in tab) {
             tab.focus();
-            tab.navigate(`${import.meta.env.VITE_BASE_URL}//chat/${senderId}`);
+            tab.navigate(
+              `${import.meta.env.VITE_FRONTEND_URL}//chat/${senderId}`,
+            );
             return;
           }
         }
